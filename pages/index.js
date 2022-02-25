786706ce640a99e50007f73e1cb456afcd463e55
Saeed-khan1 <saeed.abu@ziwo.io>
2022-02-25 17:38:17 +0400

    arabic title and meta description added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import Header from "../src/components/Header/Header";
 import CustomizedPlans from "../src/components/CustomizedPlans/CustomizedPlans";
@@ -12,16 +13,23 @@ import ConnectCrm from "../src/components/ConnectCRM/ConnectCrm";
 import WhyZiwo from "../src/components/WhyZiwo/WhyZiwo";
 import Benefits from "../src/components/Benefits/Benefits";
 
-
 export default function Home() {
+  const router = useRouter();
+  const isEnglishLocale = router.locale == "en-US" && true;
+
+  const pageTitle = isEnglishLocale
+    ? "Cloud Contact Center Software | Cloud Call Center | Cloud Calling | ZIWO |"
+    : "برنامج مركز الاتصال السحابي | مركز الاتصال السحابي | حلول مراكز الاتصال | حلول ZIWO |";
+  const pageMetaDescription = isEnglishLocale
+    ? "Cloud Contact Center Solution - API Based CRM integration - International Virtual Phone Numbers - Call tracking - Call masking - IVR"
+    : "رنامج مركز اتصالات سحابي – واجهة برمجة التطبيقات – برامج إدارة علاقات العملاء – ارقام هواتف من مخالف الدول – تتبع المكالمات – إخفاء الرقم";
+
   return (
     <>
       <Head>
-        <title>
-          Cloud Contact Center Software | Cloud Contact Center | ZIWO
-        </title>
+        <title>{pageTitle}</title>
         {/* TODO: will update the description */}
-        <meta name="description" content="Cloud Contact Center Software" />
+        <meta name="description" content={pageMetaDescription} />
         <link rel="icon" href="/images/ziwo-favicon.png" />
       </Head>
 
